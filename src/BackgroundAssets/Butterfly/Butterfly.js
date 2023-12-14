2b6155faad4c213adb556fff8439a2dba6576121
raechel <odom.raechel@gmail.com>
2023-12-13 20:17:12 -0700

    butterflystyles

--- src/BackgroundAssets/Butterfly/Butterfly.js
+++ src/BackgroundAssets/Butterfly/Butterfly.js
@@ -11,17 +11,41 @@ export const Butterfly = () => (
       <div className="body body-bulb body-bulb-bottom" />
       <div className="body body-bulb body-bulb-bottom-two" />
     </div>
-    <div className="wing left-wing-upper">
-      <div className="left-inner-wing-upper inner-wing" />
+    <div className="wing ulw">
+      <div className="ulw-innner inner-wing">
+        <div className="ulw-inner-border inner-wing-border">
+          <div className="ulw-dot-top dot" />
+          <div className="ulw-dot-mid dot" />
+          <div className="ulw-dot-bot dot" />
+        </div>
+      </div>
     </div>
-    <div className="wing left-wing-lower">
-      <div className="left-inner-wing-lower inner-wing" />
+    <div className="wing llw">
+      <div className="llw-inner inner-wing">
+        <div className="llw-inner-border inner-wing-border">
+          <div className="llw-dot-top dot" />
+          <div className="llw-dot-mid dot" />
+          <div className="llw-dot-bot dot" />
+        </div>
+      </div>
     </div>
-    <div className="wing right-wing-upper">
-      <div className="right-inner-wing-upper inner-wing" />
+    <div className="wing urw">
+      <div className="urw-inner inner-wing">
+        <div className="urw-inner-border inner-wing-border">
+          <div className="urw-dot-top dot" />
+          <div className="urw-dot-mid dot" />
+          <div className="urw-dot-bot dot" />
+        </div>
+      </div>
     </div>
-    <div className="wing right-wing-lower">
-      <div className="right-inner-wing-lower inner-wing" />
+    <div className="wing lrw">
+      <div className="lrw-inner inner-wing">
+        <div className="lrw-inner-border inner-wing-border">
+          <div className="lrw-dot-top dot" />
+          <div className="lrw-dot-mid dot" />
+          <div className="lrw-dot-bot dot" />
+        </div>
+      </div>
     </div>
   </div>
 );

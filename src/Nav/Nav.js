b8ffe4002c76b23569ce4b730d4fc5ac1d7f12bd
raechel <odom.raechel@gmail.com>
2023-12-22 10:56:15 -0700

    added more skills

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,6 +1,7 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Grid, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import WesternDivider from "../assets/western-divider.svg";
 
 export default function Nav() {
   const navigate = useNavigate();
@@ -8,20 +9,27 @@ export default function Nav() {
   return (
     <AppBar>
       <Toolbar>
-        {["about", "skills", "contact"].map((item) => (
-          <Button
-            onClick={() => {
-              navigate(`/${item}`);
-            }}
-            key={`nav-menu-${item}`}
-            sx={{ color: "#fff" }}
-          >
-            {item}
-          </Button>
-        ))}
-        <div style={{ marginLeft: "auto" }}>
-          <Typography variant="h6">raechel odom</Typography>
-        </div>
+        <Grid
+          display="flex"
+          justifyContent="space-between"
+          style={{ width: "100%" }}
+        >
+          {["about", "skills", "contact"].map((item) => (
+            <Button
+              onClick={() => {
+                navigate(`/${item}`);
+              }}
+              key={`nav-menu-${item}`}
+              sx={{ color: "#fff" }}
+              color="secondary"
+            >
+              {item}
+            </Button>
+          ))}
+          <div style={{ marginLeft: "auto" }}>
+            <Typography variant="h6">raechel odom</Typography>
+          </div>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Chip, Grid, Typography } from "@mui/material";
+import { Card, Chip, Grid, Link, Typography } from "@mui/material";
 import WesternDivider from "../assets/western-divider.svg";
 
 const skills = [
@@ -18,11 +18,14 @@ const skills = [
   "nodejs",
   "postgres",
   "jquery",
+  "d3",
+  "chartsjs",
+  "figma",
 ];
 
 const Skills = () => {
   return (
-    <Card sx={{ margin: "24px" }}>
+    <Card sx={{ margin: "24px", maxHeight: "63vh", overflowY: "scroll" }}>
       <Grid
         display="flex"
         flexWrap="wrap"
@@ -37,12 +40,31 @@ const Skills = () => {
         <img src={WesternDivider} style={{ width: "100px" }} alt="" />
       </div>
       <Grid>
-        <Typography variant="h3">Notable Accomplishments</Typography>
-        <Typography variant="subtitle1">Expanding Horizons</Typography>
         <Typography>
-          My career has been focused heavily in front-end development and
-          recently I've gotten the chance to professionally contribute to the
-          product and design decisions and weigh in on user interface.
+          I've spent three years working professionally in React/Redux and
+          before that, a year working in Angular/Ngrx. During my three years
+          working in React, my skill level has increased immensely, but I am
+          always looking for ways to improve my code and increase my knowledge.
+          <br />
+          <br />
+          In 2021 I got the idea for a pet project to help people search for
+          micro-plastic free face washes.{" "}
+          <Link href="https://github.com/raechelo/wash-your-face">
+            Wash Your Face!
+          </Link>{" "}
+          is still in development, but I've been taking the chance to explore
+          new to me frameworks, languages, and server development. The frontend
+          is built with Vue and the backend is written in Node using Express to
+          populate a Postgres managed database. The backend also leverages
+          web-scraping to find the ingredients of a searched for face-wash and
+          searches those ingredients for possible micro-plastics. The end result
+          is intended to be used as a free resource for anyone who wants to
+          avoid micro-plastics and help save the oceans and wildlife.
+          <br />
+          <br />
+          During my career, I've spent a lot of time working with charting
+          libraries such as Charts.js and D3, creating dynamic and aesthetic
+          data displays for user consumption.
         </Typography>
       </Grid>
     </Card>
